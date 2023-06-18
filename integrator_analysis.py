22eb36bedb345432bb1b33c19a9c6493274a590f
aehrenberger <74304792+Adrianoliverehren@users.noreply.github.com>
2023-06-18 14:09:07 +0200

    test

--- integrator_analysis.py
+++ integrator_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import simulation as sim
 from tudatpy.kernel.numerical_simulation import propagation_setup
+import helper_functions as hf
 
 RK_integrators_variable = {
     "rkf_45": propagation_setup.integrator.CoefficientSets.rkf_45,   
@@ -16,6 +17,12 @@ extrapolarion_integrators = {
     "deufelhard_sequence": propagation_setup.integrator.ExtrapolationMethodStepSequences.deufelhard_sequence
 }
 
+
+def gen_benchmark():
+    
+    benchmark_integrator_settings = {}
+    
+    sim.run_simulation(hf.sim_data_dir + "/integrator_analysis/benchmark", maximum_duration=1*365)
 
 
 
