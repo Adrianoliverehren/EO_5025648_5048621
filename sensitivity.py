15c06ffc3be5d1c1ee1fbc436129636c9af56729
Adrianoliverehren <74304792+Adrianoliverehren@users.noreply.github.com>
2023-06-22 22:13:39 +0200

    fixes

--- sensitivity.py
+++ sensitivity.py
@@ -15,7 +15,8 @@ def log_sensitivity(x, f, dxdf):
 
 if __name__ == '__main__':
     # Read objective values from Monte Carlo
-    t_max_arr = np.genfromtxt('./DesignSpace/monte_carlo/objectives_constraints.dat')[:, 1]
+    # t_max_arr = np.genfromtxt('./DesignSpace/monte_carlo/objectives_constraints.dat')[:, 1]
+    t_max_arr = np.genfromtxt(hf.external_sim_data_dir + "/DesignSpace/monte_carlo/objectives_constraints.dat")[:, 1]
     # Transform to days
     t_max_arr = t_max_arr / (24 * 60 * 60)
 
@@ -25,15 +26,15 @@ if __name__ == '__main__':
     v3_arr = []
     t_impulse_arr = []
     #
-    for i in range(2**12):
-        sim_dir = f'sim{i}'
-        full_dir = './DesignSpace/monte_carlo/' + sim_dir + '/'
+    for i in range(2**14):
+        sim_dir = f'iter_{i}'
+        full_dir = hf.external_sim_data_dir + '/DesignSpace/monte_carlo/' + sim_dir + '/'
         if sim_dir != 'objectives_constraints.dat' and sim_dir != 'parameter_values.dat':
             decision_var_dict = hf.create_dic_drom_json(full_dir + 'propagation_info_dic.dat')['decision_variable_dic']
 
-            v1_arr.append(decision_var_dict['dv_mag'] * decision_var_dict['dv_unit_vect'][0])
-            v2_arr.append(decision_var_dict['dv_mag'] * decision_var_dict['dv_unit_vect'][1])
-            v3_arr.append(decision_var_dict['dv_mag'] * decision_var_dict['dv_unit_vect'][2])
+            v1_arr.append(decision_var_dict['dv'][0])
+            v2_arr.append(decision_var_dict['dv'][1])
+            v3_arr.append(decision_var_dict['dv'][2])
             t_impulse_arr.append(decision_var_dict['t_impulse'])
     # Transform time to days
     t_impulse_arr = np.array(t_impulse_arr) / (24 * 60 * 60)
@@ -77,7 +78,9 @@ if __name__ == '__main__':
     plt.grid()
     plt.tight_layout()
 
-    plt.savefig('./Figures/LogSensitivity.pdf')
+    plt.savefig(hf.report_dir + '/Figures/Ch2/LogSensitivity.pdf')
+    
+    plt.show()
 
     plt.clf()
     plt.cla()
