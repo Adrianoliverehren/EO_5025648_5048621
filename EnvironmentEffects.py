89213df3267ca0ddb5ac5d4f5fb7c680acde6cf7
Augustas Barysas <A.Barysas@student.tudelft.nl>
2023-06-25 16:06:20 +0200

    Env effects update

--- EnvironmentEffects.py
+++ EnvironmentEffects.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                                 (20, 20),
                                 (40, 40)]
 
-    save_dirs = [f'./EnvironmentInv/{harmonics_to_investigate[0]}/']
+    save_dirs = [f'./EnvironmentInv/{harmonic}/' for harmonic in harmonics_to_investigate]
     n_generations = 64
     pop_size = 32
     algo = pg.de()
@@ -19,5 +19,6 @@ if __name__ == '__main__':
     x_max_lst = [1, 1, 2, 2 * 24 * 60 ** 2]
 
     for harmonics, save_dir in zip(harmonics_to_investigate, save_dirs):
+        print(harmonics, ' Running...')
         optimize(save_dir, algo, x_min_lst, x_max_lst, n_generations, pop_size, seed=42,
                  BFE=False, spherical_harmonics=harmonics)
